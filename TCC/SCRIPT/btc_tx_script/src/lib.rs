265caa9dc7cea546278cfd5a63912705a650e3ea
Thiago Machado da Silva <thi@unifei.edu.br>
2017-09-20 23:39:12 +0000

    teste dos trait

--- TCC/SCRIPT/btc_tx_script/src/lib.rs
+++ TCC/SCRIPT/btc_tx_script/src/lib.rs
@@ -4,6 +4,8 @@ extern crate byteorder;
 extern crate arrayvec;
 use std::io::Cursor;
 use std::fmt;
+//use std::io::{Error, ErrorKind};
+use std::error::Error;
 
 //use hex::FromHex;
 use byteorder::{LittleEndian, ReadBytesExt};
@@ -61,14 +63,48 @@ impl std::fmt::Debug for Bytes {
   }
 }
 
+pub struct Msg {
+  pub header: MsgHeader,
+  pub payload: Option<Box<std::fmt::Debug>>,
+}
 
+impl Msg {
+  pub fn new(it: &mut std::vec::IntoIter<u8>) -> Msg {
+
+    let header = MsgHeader::new(it);
+    let payload = match header.cmd.clone().into_iter()
+      .map(|x| x as char).collect::<String>().as_ref() {
+
+      "tx" => Some(Box::new(Tx::new(it).unwrap())),
+      "ping" => Some(Box::new(Ping::new(it).unwrap())),
+      "pong" => Some(Box::new(Pong::new(it).unwrap())),
+      _ => None,
+    };
+
+    // header.payload_len // TODO
+
+    Msg {
+      header: header,
+      payload: payload,
+    }
+  }
+
+}
+
+/*
+pub enum MsgPayload {
+  Tx,
+  Ping,
+  Pong,
+}*/
+
+// https://en.bitcoin.it/wiki/Protocol_documentation#tx
 pub struct MsgHeader {
   pub network: u32,
   pub cmd: ArrayVec<[u8; 12]>,
-  pub payload: i32,
+  pub payload_len: i32,
   pub payloadchk: u32,
 }
-
 
 impl MsgHeader {
   pub fn new(it: &mut std::vec::IntoIter<u8>) -> MsgHeader {
@@ -76,7 +112,7 @@ impl MsgHeader {
       network: Cursor::new(it.take(4).collect::<Vec<u8>>())
         .read_u32::<LittleEndian>().unwrap(),
       cmd: it.take(12).map(|u| u.to_le()).collect::<ArrayVec<[u8; 12]>>(),
-      payload: Cursor::new(it.take(4).collect::<Vec<u8>>())
+      payload_len: Cursor::new(it.take(4).collect::<Vec<u8>>())
         .read_i32::<LittleEndian>().unwrap(),
       payloadchk: Cursor::new(it.take(4).collect::<Vec<u8>>())
         .read_u32::<LittleEndian>().unwrap(),
@@ -89,15 +125,70 @@ impl std::fmt::Debug for MsgHeader {
   fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
       let mut s = "Message Header:\n".to_string();
       s += &format!("├ Message Network Identification: {}\n", self.network);
-      s += &format!("├ Message Command OP_CODE: {:?}\n", self.cmd
-        .clone().into_iter().collect::<Bytes>());
-      s += &format!("├ Payload: {}\n", self.payload);
+      s += &format!("├ Message Command OP_CODE: <{}>{:?}\n",
+        self.cmd.clone().into_iter().map(|x| x as char).collect::<String>(),
+        self.cmd.clone().into_iter().collect::<Bytes>());
+      //str::from_utf8
+      s += &format!("├ Payload Length: {}\n", self.payload_len);
       s += &format!("├ Payload Checksum: {}\n", self.payloadchk);
 
       write!(f, "{}", s)
   }
 }
 
+
+// https://bitcoin.org/en/developer-reference#ping
+pub struct Ping {
+  pub nounce: u64,
+}
+
+impl Ping {
+  //pub fn new(it: &mut std::vec::IntoIter<u8>) -> Result<Ping, Box<Error>> {
+  pub fn new(it: &mut std::vec::IntoIter<u8>) -> Box<std::dmt::Debug> {
+
+    let nounce = Cursor::new(it.take(8).collect::<Vec<u8>>())
+          .read_u64::<LittleEndian>()?;
+    Ok(Ping {
+      nounce: nounce,
+    })
+  }
+}
+
+impl std::fmt::Debug for Ping {
+  fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
+      let mut s = "Ping:\n".to_string();
+      s += &format!("├ Nounce: {}\n", self.nounce);
+      write!(f, "{}", s)
+  }
+}
+
+// https://bitcoin.org/en/developer-reference#ping
+pub struct Pong {
+  pub nounce: u64,
+}
+
+impl Pong {
+  //pub fn new(it: &mut std::vec::IntoIter<u8>) -> Result<Pong, Box<Error>> {
+  pub fn new(it: &mut std::vec::IntoIter<u8>) -> Box<std::fmt::Debug> {
+
+    let nounce = Cursor::new(it.take(8).collect::<Vec<u8>>())
+          .read_u64::<LittleEndian>()?;
+    Ok(Pong {
+      nounce: nounce,
+    })
+  }
+}
+
+impl std::fmt::Debug for Pong {
+  fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
+      let mut s = "Pong:\n".to_string();
+      s += &format!("├ Nounce: {}\n", self.nounce);
+      write!(f, "{}", s)
+  }
+}
+
+// https://en.bitcoin.it/wiki/Protocol_documentation#tx
+// https://bitcoin.org/en/developer-reference#raw-transaction-format
 pub struct Tx {
   pub version: i32,
   pub inputs_len: u8,
@@ -109,39 +200,47 @@ pub struct Tx {
 }
 
 impl Tx {
-  pub fn new(it: &mut std::vec::IntoIter<u8>) -> Tx {
+  //pub fn new(it: &mut std::vec::IntoIter<u8>) -> Result<Tx, Box<Error>> {
+  pub fn new(it: &mut std::vec::IntoIter<u8>) -> Box<std::fmt::Debug> {
     let ver = Cursor::new(it.by_ref().take(4).collect::<Vec<u8>>())
-      .read_i32::<LittleEndian>().unwrap();
+      .read_i32::<LittleEndian>()?;
 
-    let ninputs = it.by_ref().next().unwrap().to_le();
+    let ninputs = it.by_ref().next().ok_or("TODO")?.to_le();
     let mut inputs: Vec<TxInput> = vec![];
     for _ in 0..ninputs {
       inputs.push(TxInput::new(it));
     }
 
-    let noutputs = it.by_ref().next().unwrap().to_le();
+    let noutputs = it.by_ref().next().ok_or("TODO")?.to_le();
     let mut outputs: Vec<TxOutput> = vec![];
     for _ in 0..noutputs {
       outputs.push(TxOutput::new(it));
     }
 
-    Tx {
+    let locktime = Cursor::new(it.take(4).collect::<Vec<u8>>())
+          .read_u32::<LittleEndian>()?;
+
+    let tx = Tx {
       version: ver,
       inputs_len: ninputs,
       inputs: inputs,
       outputs_len: noutputs,
       outputs: outputs,
-      locktime: Cursor::new(it.take(4).collect::<Vec<u8>>())
-          .read_u32::<LittleEndian>().unwrap(),
+      locktime: locktime,
+    };
+    if let Some(_) = it.next() {
+      Err("TODO")?;
     }
-
+    Ok(tx)
   }
 }
+
 
 impl std::fmt::Debug for Tx {
   fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
       let mut s = "Tx:\n".to_string();
       s += &format!("├ Version: {}\n", self.version);
+      s += &format!("├ Inputs Length: {}\n", self.inputs_len);
       s += &format!("├ Inputs:\n");
       for (i, input) in self.inputs.iter().enumerate() {
         s += &format!(" {:?}", input)
@@ -159,6 +258,7 @@ impl std::fmt::Debug for Tx {
             })
           .collect::<String>();
       }
+      s += &format!("├ Outputs Length: {}\n", self.outputs_len);
       s += &format!("├ Outputs:\n");
       for (i, output) in self.outputs.iter().enumerate() {
         s += &format!(" {:?}", output)
@@ -193,6 +293,7 @@ pub struct TxInput {
 }
 
 impl TxInput {
+  pub fn new(it: &mut std::vec::IntoIter<u8>) -> TxInput {
   pub fn new(it: &mut std::vec::IntoIter<u8>) -> TxInput {
       let ptx = it.take(32).map(|u| u.to_le()).collect::<ArrayVec<[u8; 32]>>();
       let ptxoi = Cursor::new(it.take(4).collect::<Vec<u8>>())
--- TCC/SCRIPT/btc_tx_script/src/main.rs
+++ TCC/SCRIPT/btc_tx_script/src/main.rs
@@ -7,18 +7,35 @@ use btc_tx_script as btctx;
 fn main() {
 
   // example tx msg
+  // https://en.bitcoin.it/wiki/Protocol_documentation#tx
   let tx_msg = "F9BEB4D974780000000000000000000002010000E293CDBE01000000016DBDDB085B1D8AF75184F0BC01FAD58D1266E9B63B50881990E4B40D6AEE3629000000008B483045022100F3581E1972AE8AC7C7367A7A253BC1135223ADB9A468BB3A59233F45BC578380022059AF01CA17D00E41837A1D58E97AA31BAE584EDEC28D35BD96923690913BAE9A0141049C02BFC97EF236CE6D8FE5D94013C721E915982ACD2B12B65D9B7D59E20A842005F8FC4E02532E873D37B96F09D6D4511ADA8F14042F46614A4C70C0F14BEFF5FFFFFFFF02404B4C00000000001976A9141AA0CD1CBEA6E7458A7ABAD512A9D9EA1AFB225E88AC80FAE9C7000000001976A9140EAB5BEA436A0484CFAB12485EFDA0B78B4ECC5288AC00000000";
   let vec_msg: Vec<u8> = Vec::from_hex(tx_msg).unwrap();
-
-  /*for u in &vec_msg {
-    print!("{}-", u);
-  }
-  println!("");*/
-
   let mut it = vec_msg.into_iter();
   let header = btctx::MsgHeader::new(it.by_ref());
   println!("\n{:?}", header);
-  let tx = btctx::Tx::new(it.by_ref());
+  let tx = btctx::Tx::new(it.by_ref()).unwrap();
   println!("{:?}", tx);
 
+  // https://bitcoin.org/en/developer-examples#simple-raw-transaction
+  //let tx_raw_hex = "01000000017b1eabe0209b1fe794124575ef807057c77ada2138ae4fa8d6c4de0398a14f3f0000000000ffffffff01f0ca052a010000001976a914cbc20a7664f2f69e5355aa427045bc15e7c6c77288ac00000000";
+  // ...
+
+  // https://bitcoin.org/en/developer-examples#complex-raw-transaction
+  let tx_raw_hex = "0100000002f327e86da3e66bd20e1129b1fb36d07056f0b9a117199e759396526b8f3a20780000000000fffffffff0ede03d75050f20801d50358829ae02c058e8677d2cc74df51f738285013c260000000000ffffffff02f028d6dc010000001976a914ffb035781c3c69e076d48b60c3d38592e7ce06a788ac00ca9a3b000000001976a914fa5139067622fd7e1e722a05c17c2bb7d5fd6df088ac00000000";
+  let tx_raw_hex_vec_msg: Vec<u8> = Vec::from_hex(tx_raw_hex).unwrap();
+  let mut it2 = tx_raw_hex_vec_msg.into_iter();
+  let tx2 = btctx::Tx::new(it2.by_ref()).unwrap();
+  println!("{:?}", tx2);
+
+  //0094102111e2af4d
+
+  let pl_ping_hex = "0094102111e2af4d";
+  let pl_ping_hex_vec: Vec<u8> = Vec::from_hex(pl_ping_hex).unwrap();
+
+  let mut pl_ping_it = pl_ping_hex_vec.into_iter();
+  let pl_ping = btctx::Ping::new(pl_ping_it.by_ref()).unwrap();
+  println!("{:?}", pl_ping);
+
+
 }
+
