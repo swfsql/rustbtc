8d5ce1ec0d834977b1273e421d9fadbe7ae96a09
Thiago Machado da Silva <thi@unifei.edu.br>
2018-03-20 20:15:54 +0000

    admin server

--- Software/btc/src/main.rs
+++ Software/btc/src/main.rs
@@ -28,10 +28,30 @@ extern crate tokio;
 extern crate bytes;
 extern crate futures;
 
+#[macro_use]
+extern crate structopt;
+
 use tokio::net::{TcpListener, TcpStream};
 use tokio::prelude::*;
 
-fn process(socket: TcpStream) {
+use std::thread;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+fn process_peer(socket: TcpStream) {
+    let peer = btc::peer::Peer::new(socket);
+
+    //        .map_err(|_| ());
+
+    let peer_machina = btc::peer::machina::Machina::start(peer)
+        .map_err(|_| ())
+        .map(|_| ());
+
+    tokio::spawn(peer_machina);
+    println!("depois do spawn");
+}
+fn process_admin(socket: TcpStream) {
     let peer = btc::peer::Peer::new(socket);
 
     //        .map_err(|_| ());
@@ -54,22 +74,39 @@ fn run() -> Result<()> {
         time::now().strftime("%Hh%Mm%Ss - D%d/M%m/Y%Y").unwrap()
     );
 
-    let addr = "127.0.0.1:8080".parse().unwrap();
+    let addr_peer = "127.0.0.1:8080".parse().unwrap();
+    let addr_admin = "127.0.0.1:8081".parse().unwrap();
 
-    let listener = TcpListener::bind(&addr).unwrap();
+    let listener_peer = TcpListener::bind(&addr_peer).unwrap();
+    let listener_admin = TcpListener::bind(&addr_admin).unwrap();
 
-    let server = listener
+    let server_peer = listener_peer
         .incoming()
         .for_each(move |socket| {
-            process(socket);
+            process_peer(socket);
+            Ok(())
+        })
+        .map_err(|err| {
+            println!("accept error = {:?}", err);
+        });
+    let server_admin = listener_admin
+        .incoming()
+        .for_each(move |socket| {
+            process_admin(socket);
             Ok(())
         })
         .map_err(|err| {
             println!("accept error = {:?}", err);
         });
 
-    println!("server running on localhost:8080");
-    tokio::run(server);
+    println!("server_peer running on localhost:8080");
+    println!("server_admin running on localhost:8081");
+
+    thread::spawn(move || {
+        tokio::run(server_admin);
+    });
+
+    tokio::run(server_peer);
 
     info!(
         "\n\
